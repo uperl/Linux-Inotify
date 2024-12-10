e9aae8db738fe5bf14f044690f66299b67894415
Graham Ollis <plicease@cpan.org>
2024-12-10 14:32:49 -0700

    refactor: remove prototypes, use require instead of use

--- lib/Linux/Inotify/Event.pm
+++ lib/Linux/Inotify/Event.pm
@@ -3,7 +3,7 @@ package Linux::Inotify::Event;
 use strict;
 use warnings;
 
-sub new($$$) {
+sub new {
    my $class = shift;
    my $notifier = shift;
    my $raw_event = shift;
@@ -19,43 +19,40 @@ sub new($$$) {
    return bless $self, $class;
 }
 
-sub fullname($) {
+sub fullname {
    my $self = shift;
    return $self->{watch}->{name} . '/' . $self->{name};
 }
 
-sub add_watch($) {
+sub add_watch {
    my $self = shift;
    return $self->{watch}->clone($self->fullname());
 }
 
-my %reverse;
-
-INIT {
-   %reverse = (
-      0x00000001 => 'access',
-      0x00000002 => 'modify',
-      0x00000004 => 'attrib',
-      0x00000008 => 'close_write',
-      0x00000010 => 'close_nowrite',
-      0x00000020 => 'open',
-      0x00000040 => 'moved_from',
-      0x00000080 => 'moved_to',
-      0x00000100 => 'create',
-      0x00000200 => 'delete',
-      0x00000400 => 'delete_self',
-      0x00002000 => 'unmount',
-      0x00004000 => 'q_overflow',
-      0x00008000 => 'ignored',
-   );
-   my %reverse_copy = %reverse;
-   while(my ($key, $value) = each %reverse_copy) {
-      use Linux::Inotify;
-      $reverse{Linux::Inotify::ISDIR | $key} = "isdir | $value";
-   }
+my %reverse = (
+   0x00000001 => 'access',
+   0x00000002 => 'modify',
+   0x00000004 => 'attrib',
+   0x00000008 => 'close_write',
+   0x00000010 => 'close_nowrite',
+   0x00000020 => 'open',
+   0x00000040 => 'moved_from',
+   0x00000080 => 'moved_to',
+   0x00000100 => 'create',
+   0x00000200 => 'delete',
+   0x00000400 => 'delete_self',
+   0x00002000 => 'unmount',
+   0x00004000 => 'q_overflow',
+   0x00008000 => 'ignored',
+);
+my %reverse_copy = %reverse;
+while(my ($key, $value) = each %reverse_copy) {
+   require Linux::Inotify;
+   $reverse{Linux::Inotify::ISDIR | $key} = "isdir | $value";
 }
 
-sub print(%) {
+
+sub print {
    my $self = shift;
    printf "fd: %d, wd: %d, %21s, cookie: 0x%08x, len: %3d, name: '%s'\n",
       $self->{notifier}->{fd}, $self->{watch}->{wd}, $reverse{$self->{mask}},
@@ -63,4 +60,3 @@ sub print(%) {
 }
 
 1;
-
